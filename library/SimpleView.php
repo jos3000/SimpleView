e4e3fda5a1a27a0f7d0141df84bc802264daabe9
Jos Shepherd <jos@hipsnip.com>
2010-05-03 19:13:22 +0100

    Basic bugfixes

--- library/SimpleView.php
+++ library/SimpleView.php
@@ -4,64 +4,75 @@ class SimpleView {
 
 	private $store = array();
 	private $page;
-	private $current;
+
 	private $include_paths = array('.');
 	private $extensions = array('','.php','.html.php');
 	
-	public function __construct($include_paths == false){
+	public function __construct($include_paths = false){
 	    if($include_paths){
-	        $this->include_paths = array_merge($this->include_paths, $include_paths);
+			if(!is_array($include_paths)) $include_paths = array($include_paths);
+			$this->include_paths = array_merge($this->include_paths, $include_paths);
+			
 	    }
 	}
 	
-	public function run($page, $template == false){
+	public function run($page, $template = false){
 	
-		$this->current = $this->page = $page;
+		$this->page = $page;
 		$this->store = array(); # clean out the store
 		
 		if(!$template && isset($page['template'])) $template = $page['template'];
 		else throw new Exception('Template not defined');
 		
-		return $this->include($template);
+		return $this->inc($template);
 	}
 	
-	private function include($template, $current = false){
+	private function inc($template, $current = false){
+		
+		$__ = $this->page;
 		
 		if($current !== false){
-			$this->current = $current;
+			$_ = $current;
+		} else {
+			$_ = $__;
 		}
 		
 		unset($current);
 		
-		$_ = $this->current;
-		$__ = $this->page;
-		
-		ob_start()
+		ob_start();
 		require($this->resolvePath($template));
-		return ob_end_clean();
+		$return = ob_get_contents();
+		ob_end_clean();
+		return $return;
 	
 	}
 	
 	private function store($name){
 	
-		$store['name'] = ob_end_clean();
+		$this->store[$name] = ob_get_contents();
+		ob_end_clean();
 		ob_start();
 	
 	}
 	
 	private function retrieve($name){
-		if(isset($store['name'])) return $store['name'];
+		if(isset($this->store[$name])) return $this->store[$name];
 	
 	}
 	
     private function resolvePath($template){
-        foreach($this->include_paths AS $p){
-            foreach($this->extensions AS $e){
-                $t = $p.'/'.$template.$e;
-                if(file_exists($t)) return $t;
-            }
-        }
-        
-    }	
+
+		foreach($this->include_paths AS $p){
+			foreach($this->extensions AS $e){
+				$t = $p.'/'.$template.$e;
+				if(file_exists($t)) return $t;
+			}
+		}
+		
+		# template not found - throw Exception
+		throw new Exception('Template for ['.$template.'] not found.');
+		
+	}
+	
 
 }
